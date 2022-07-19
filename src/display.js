4df1fe7570cbf23e3ce30c99a2c12a992194fba7
ThiagoJoseSousa <imigrationstudy@gmail.com>
2022-07-19 11:45:37 -0300

    weather now gets data by form

--- src/display.js
+++ src/display.js
@@ -1,14 +1,13 @@
 const displayLastSearch = () => {
     let divLastSearch= document.getElementById("currentData");
     let lastElement = weatherObjects[weatherObjects.length-1];
-    let txt = "";
-    //for in is a builtin syntax that continues until the keys end.
-for (let x in lastElement) {
+  
+    //for in is a builtin loop syntax that continues until the keys end. innerHTML is trustable 
+    //and will overwrite previous text
+    let txt="";
+  for (let x in lastElement) {
   txt += x + ": " + lastElement[x] + "<br>";
-};
-divLastSearch.innerHTML=txt;
-}
+  };
+  divLastSearch.innerHTML=txt;
+  }
 
-const updatePageOnSearch ='';
-
-const clearLastDiv='';
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const initializeForm= () => {let form = document.querySelector("#form");
 
-form.addEventListener("click", function () {
+form.addEventListener("submit", function (e) {
+e.preventDefault();
 let formdata = new FormData(this);
 let input = formdata.get("inputbox");
 let temperatureUnit= formdata.get("temperatureUnit");
 
+getWeather(input,temperatureUnit);
 });} 
-
+initializeForm();
 //factory function, clear div, add to div, displaying It. 
 // I will make an array to store the objects created by weather factory, using the scope of a function It'll be easy to create without repeating name
 
@@ -24,9 +26,9 @@ weatherObjects.push(lastObject);
 }
 
 
-const getWeather = () => {
-let cityname="Carapicuiba";
-let temperatureUnit="metric";
+const getWeather = (input, temperature) => {
+let cityname=input;
+let temperatureUnit=temperature;
 
 fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=8b4e96d7d03aca7a84c60917ff96fa26&units=${temperatureUnit}`, 
 { mode:'cors'})
@@ -42,21 +44,11 @@ fetch (`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=8b4e
     let humidity=response.main.humidity;
   
 pushObjects(weather,temp,temp_max,temp_min,pressure,humidity);
-displayLastSearch();
+ displayLastSearch();
 
   });
 
 } // when getWeather ends pushObjects must happen.
-getWeather();
 
-const displayLastSearch = () => {
-  let divLastSearch= document.getElementById("currentData");
-  let lastElement = weatherObjects[weatherObjects.length-1];
-  let txt = "";
-  //understand below
-for (let x in lastElement) {
-txt += x + ": " + lastElement[x] + "<br>";
-};
-divLastSearch.innerHTML=txt;
-}
+
 
